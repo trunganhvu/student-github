472e71aef9edac6f5c11177d10f70bf9e2f892fa
Trung Anh <vutrunganh1@gmail.com>
2024-06-18 15:19:14 +0700

    update api

--- src/main/java/com/example/studentgithub/controller/StudentController.java
+++ src/main/java/com/example/studentgithub/controller/StudentController.java
@@ -20,6 +20,7 @@ public class StudentController {
         students.add(new Student(1, "John", "12A", 17, "123 Main St", 85, 75, 90, 80,90));
         students.add(new Student(2, "Jane", "11B", 16, "456 Park Ave", 90, 80, 85, 75,89));
         students.add(new Student(3, "Alice", "10C", 15, "789 Oak St", 80, 85, 70, 90,78));
+        students.add(new Student(4, "Zoey", "10C", 15, "789 Oak St", 80, 85, 70, 90,78));
 
         return students;
     }
